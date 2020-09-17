3d4da0df6118068b286e979a2111bb36e693f3bc
Animesh Sinha <AnimeshSinha1309@gmail.com>
2020-09-17 05:43:10 +0530

    Corrected test file path
    QASM file path corrected, now everything is accessed from root dir. Cleaned up useless lines in CI.

--- test/test_circuit.py
+++ test/test_circuit.py
@@ -2,7 +2,7 @@ import qroute as q
 
 
 def test_dag_from_qasm():
-    c = q.circuits.load_circuit.dag_from_qasm('circuit_qasm/3_17_13_onlyCX.qasm')
+    c = q.circuits.load_circuit.dag_from_qasm('test/circuit_qasm/3_17_13_onlyCX.qasm')
     assert c.number_of_nodes() == 17
     assert c.number_of_edges() == 136
 
