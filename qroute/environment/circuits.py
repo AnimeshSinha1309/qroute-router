5a532d07179b0da79b21a9d7de4da5789d407e18
Animesh Sinha <animeshsinha1309@gmail.com>
2020-09-20 04:48:44 +0530

    Very basic circuit state
    Only the circuit maintains state, that too just active gates at the leaf. Added a test to make sure it works.

--- qroute/environment/circuits.py
+++ qroute/environment/circuits.py
@@ -1,5 +1,5 @@
 import cirq
-import collections
+import networkx as nx
 import numpy as np
 
 
@@ -7,26 +7,45 @@ class CircuitState:
 
     def __init__(self, circuit: cirq.Circuit):
         self.circuit = circuit
+        # TODO: Make this DAG object global and shared between all instances, immutable
         self.dag = cirq.CircuitDag.from_circuit(self.circuit)
-        self.qubits = list(sorted(self.circuit.all_qubits()))
         self.dag_nodes = list(self.dag.nodes)
         self.__node_to_index = {node: idx for idx, node in enumerate(self.dag_nodes)}
-        # Making a list of tuples [g_i = (i, j, ...)] where i, j, ... are the qubits operated on
-        self.operations = []
-        for node in self.dag.nodes:
-            operation: cirq.Operation = node.val
-            self.operations.append(tuple(map(self.qubits.index, operation.qubits)))
         # Make the queue of leaf nodes
-        self.__queue = collections.deque()
-        self.__indegree = np.zeros(shape=self.dag.number_of_nodes())
+        self.__leaf_queue: set = set()
+        self.__indegree = np.zeros(shape=self.dag.number_of_nodes(), dtype=np.int16)
         for u in self.dag_nodes:
             for v in self.dag.successors(u):
                 self.__indegree[self.__node_to_index[v]] += 1
         for idx, indegree in enumerate(self.__indegree):
             if self.__indegree[idx] == 0:
-                self.__queue.append(idx)
-        print(self.__indegree)
-        print(self.__queue)
+                self.__leaf_queue.add(idx)
+
+    def draw_circuit_graph(self):
+        nx.draw(self.dag, with_labels=True)
+
+    def pop(self, n):
+        for v in self.dag.successors(self.dag_nodes[n]):
+            idx = self.__node_to_index[v]
+            self.__indegree[idx] -= 1
+            if self.__indegree[idx] == 0:
+                self.__leaf_queue.add(idx)
+        self.__leaf_queue.remove(n)
+
+    @property
+    def leaf_nodes(self):
+        return self.__leaf_queue.copy()
+
+    @property
+    def operations(self):
+        # Making a list of tuples [g_i = (i, j, ...)] where i, j, ... are
+        # the qubits operated on in the i-th gate, gates in node order
+        qubits = list(sorted(self.circuit.all_qubits()))
+        operations = []
+        for node in self.dag_nodes:
+            operation: cirq.Operation = node.val
+            operations.append(tuple(map(qubits.index, operation.qubits)))
+        return operations
 
 
 def dag_from_qasm(filename):
@@ -50,4 +69,4 @@ def circuit_from_qasm(filename):
     return circuit
 
 
-c = CircuitState(circuit=circuit_from_qasm('test/circuit_qasm/3_17_13.qasm'))
+c = CircuitState(circuit=circuit_from_qasm('test/circuit_qasm/test.qasm'))
--- test/test_circuit.py
+++ test/test_circuit.py
@@ -1,13 +1,36 @@
-from qroute import environment
+from qroute import environment as env
+
+
+def test_circuit_pop():
+    c = env.circuits.CircuitState(circuit=env.circuits.circuit_from_qasm('test/circuit_qasm/test.qasm'))
+    assert c.leaf_nodes == {0, 1, 2, 3}
+    c.pop(1)
+    assert c.leaf_nodes == {0, 2, 3, 4}
+    c.pop(0)
+    assert c.leaf_nodes == {2, 3, 4}
+    c.pop(4)
+    assert c.leaf_nodes == {2, 3, 8}
+    c.pop(8)
+    assert c.leaf_nodes == {2, 3}
+    c.pop(2)
+    assert c.leaf_nodes == {3, 5}
+    c.pop(3)
+    assert c.leaf_nodes == {5, 6}
+    c.pop(6)
+    assert c.leaf_nodes == {5}
+    c.pop(5)
+    assert c.leaf_nodes == {7}
+    c.pop(7)
+    assert c.leaf_nodes == set()
 
 
 def test_dag_from_qasm():
-    c = environment.circuits.dag_from_qasm('test/circuit_qasm/3_17_13_onlyCX.qasm')
+    c = env.circuits.dag_from_qasm('test/circuit_qasm/3_17_13_onlyCX.qasm')
     assert c.number_of_nodes() == 17
     assert c.number_of_edges() == 136
 
 
 def test_generate_random_cirq_circuit():
-    c = environment.generate.generate_random_cirq_circuit(5, 20, 0.8)
+    c = env.generate.generate_random_cirq_circuit(5, 20, 0.8)
     assert len(list(c.all_qubits())) == 5
     assert len(list(c.all_operations())) >= 20  # All operations = 2-Qubits-Op + 3 * (1-Qubit-Op) >= 20
