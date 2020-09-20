3393b72f74a059499dd9851dc66fc3ee8864a85b
Animesh Sinha <animeshsinha1309@gmail.com>
2020-09-20 21:19:09 +0530

    Popping operations evolves Circuit state
    The state of the circuit evolves as we pop operations from it, both in the leaf nodes representation and the next-op neighbor representation (implemented as two parallel tasks now).
    
    Renders significant progress to #5.

--- qroute/environment/device.py
+++ qroute/environment/device.py
@@ -1,12 +1,16 @@
 import cirq
 import networkx as nx
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 class DeviceTopology(cirq.Device):
 
-    def __init__(self):
+    def __init__(self, nodes, edges):
         self.graph = nx.DiGraph()
+        self.graph.add_nodes_from(nodes)
+        self.graph.add_edges_from(edges)
+        self.distances = self.__get_distance_matrix()
 
     def __get_distance_matrix(self):
         """
@@ -39,22 +43,21 @@ class DeviceTopology(cirq.Device):
                 self.validate_operation(operation)
 
     def draw_architecture_graph(self):
-        nx.draw(self.graph, with_labels=True)
+        nx.draw(self.graph, pos=nx.circular_layout(self.graph), with_labels=False)
+        plt.show()
 
 
 class IBMqx3Device(DeviceTopology):
 
     def __init__(self):
-        super(IBMqx3Device, self).__init__()
-        self.graph.add_nodes_from(list(range(16)))
-        self.graph.add_edges_from([(1, 2), (1, 0), (2, 3), (3, 4), (3, 14), (5, 4), (6, 5), (6, 11), (6, 7), (7, 10),
-                                   (8, 7), (9, 8), (9, 10), (11, 10), (12, 5), (12, 11), (12, 13), (13, 14), (13, 4),
-                                   (15, 14), (15, 2), (15, 0)])
-        self.distances = self.__get_distance_matrix()
+        super(IBMqx3Device, self).__init__(
+            nodes=list(range(16)),
+            edges=[(1, 2), (1, 0), (2, 3), (3, 4), (3, 14), (5, 4), (6, 5), (6, 11),
+                   (6, 7), (7, 10), (8, 7), (9, 8), (9, 10), (11, 10), (12, 5), (12, 11),
+                   (12, 13), (13, 14), (13, 4), (15, 14), (15, 2), (15, 0)]
+        )
 
 
 if __name__ == "__main__":
-    from matplotlib import pyplot as plt
     device = IBMqx3Device()
     device.draw_architecture_graph()
-    plt.show()
